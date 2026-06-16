4e541957f1c53d2ce8af5e5db18354a8a8b2a57c
Martha <marthasibiya25@gmail.com>
2026-06-16 18:32:02 +0200

    save

--- SafeConnect/js/contacts.js
+++ SafeConnect/js/contacts.js
@@ -1,12 +1,64 @@
-export function getContacts() {
-    return JSON.parse(localStorage.getItem("contacts")) || [];
+const contactForm = document.getElementById("contactForm");
+const contactList = document.getElementById("contactList");
+
+let contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+displayContacts();
+
+contactForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name =
+        document.getElementById("contactName").value;
+
+    const phone =
+        document.getElementById("contactPhone").value;
+
+    const newContact = {
+        name,
+        phone
+    };
+
+    contacts.push(newContact);
+
+    localStorage.setItem(
+        "contacts",
+        JSON.stringify(contacts)
+    );
+
+    contactForm.reset();
+
+    displayContacts();
+});
+
+function displayContacts() {
+
+    contactList.innerHTML = "";
+
+    contacts.forEach((contact, index) => {
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+            ${contact.name} - ${contact.phone}
+            <button onclick="deleteContact(${index})">
+                Delete
+            </button>
+        `;
+
+        contactList.appendChild(li);
+    });
 }
 
-export function addContact(contact) {
+function deleteContact(index) {
 
-    let contacts = getContacts();
+    contacts.splice(index, 1);
 
-    contacts.push(contact);
+    localStorage.setItem(
+        "contacts",
+        JSON.stringify(contacts)
+    );
 
-    localStorage.setItem("contacts", JSON.stringify(contacts));
+    displayContacts();
 }
